868191ab0ff1e2d096a0c7844815e7a2388742e8
Muhammad Abdullah Khan <abdullah.khan10032@gmail.com>
2024-12-23 19:34:51 +0100

    Make Code Work with new folder structure

--- src/shared/main.py
+++ src/shared/main.py
@@ -8,8 +8,8 @@
 #print("Current working directory\n" + os.getcwd())
 
 import pandas                        as pd
-from core import methods             as m1
-from core import HelperTools         as ht
+import methods             as m1
+import HelperTools         as ht
 
 from config                          import pdict
 
@@ -18,13 +18,13 @@ from config                          import pdict
 def main():
     """Main: Generation of Streamlit App for visualizing electric charging stations & residents in Berlin"""
 
-    df_geodat_plz   = pd.read_csv('datasets/geodata_berlin_plz.csv', delimiter = ';')
+    df_geodat_plz   = pd.read_csv('infrastructure/datasets/geodata_berlin_plz.csv', delimiter = ';')
     
-    df_lstat        = pd.read_csv('charging_stations_updated.csv')
+    df_lstat        = pd.read_csv('infrastructure/datasets/charging_stations_updated.csv')
     df_lstat2       = m1.preprop_lstat(df_lstat, df_geodat_plz, pdict)
     gdf_lstat3      = m1.count_plz_occurrences(df_lstat2)
     
-    df_residents    = pd.read_csv('plz_einwohner.csv')
+    df_residents    = pd.read_csv('infrastructure/datasets/plz_einwohner.csv')
     gdf_residents2  = m1.preprop_resid(df_residents, df_geodat_plz, pdict)
     
     m1.make_streamlit_electric_Charging_resid(gdf_lstat3, gdf_residents2)
